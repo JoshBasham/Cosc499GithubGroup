8a656e612e8a8c95523afb540ece3ad21a89a92f
Joels17 <joel.semeniuk@gmail.com>
2021-09-18 14:52:21 -0700

    Fix

---
+++ arrayFunctions.py
@@ -0,0 +1,14 @@
+
+from scripts.arraySum import arraySum
+from scripts.arrayMean import arrayMean
+
+
+nums = [3, 6, 9, 13, 17, 23, 40, 20, 13, 8]
+
+print(nums)
+print("Using mean function: ")
+print(arrayMean(nums))
+print("Using mean function: ")
+print(arrayMean(nums))
+
+
