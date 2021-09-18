f62e6a0ddbc434024da9854fb149d36e1d2a7e8e
Zmontgomery96 <Zacharymontgomery1996@gmail.com>
2021-09-18 13:41:27 -0700

    working test function

--- tests/test_arrayMedian.py
+++ tests/test_arrayMedian.py
@@ -1,10 +1,8 @@
 from scripts.median import median
 import unittest
 
-from scripts import median
-
 class TestMedian(unittest.TestCase):
     def testMedian(self):
-        actual = median(nums=["3","5","4"])
-        excepted = {"5"}
+        actual = median(nums=[3, 6, 9, 13, 17, 23, 40, 20, 13, 8])
+        excepted = 20.0
         self.assertEqual(actual, excepted)
